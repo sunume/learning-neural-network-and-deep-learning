91b6aa3bb6c3289f453685866f8404887bb6755e
sunume <1281992309@qq.com>
2018-12-07 10:58:42 +0800

    better visualization of heatmap (thanks to Ram!) Merge pull request #158 from rasmusbergpalm/deprecation
    Add deprecation notice
    🌃 🤔 💭 👹
    Update DATASET.md

--- exercises/sigmoidtest.py
+++ exercises/sigmoidtest.py
@@ -12,11 +12,11 @@ for i in range(1,11):
     biases = 3*i
     output = sum(inputVec1*weight) +biases
     output = 1.0/(1+math.exp(-output*1.0))
-    print output
+    print(output)
     
 for i in range(1,11):
     weight = array([-2*i,-2*i])
     biases = -3*i
     output = sum(inputVec2*weight) +biases
     output = 1.0/(1+math.exp(-output*1.0))
-    print output
+    print(output)
